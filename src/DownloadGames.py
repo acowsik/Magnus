b68232098f7514fcd67284071cd5ccaaeadd45e8
Aditya Cowsik <aditya.cowsik@gmail.com>
2016-05-08 20:07:08 -0500

    Now the download file actually works... Have to make sure that the gitignore works

--- src/DownloadGames.py
+++ src/DownloadGames.py
@@ -3,6 +3,9 @@
 import bz2
 import requests
 import datetime
+import os
+import errno
+import urllib2
 
 
 for year in xrange(1999, datetime.datetime.now().year):
@@ -15,13 +18,23 @@ for year in xrange(1999, datetime.datetime.now().year):
         'download': 'Download'
     })
 
-    print r.text
+    if 'query quota exceeded' in r.text:
+        print "Wait a little, I don't know how long"
+        break
 
     urlstart = r.text.index('/dl/')
     urlend = r.text.index('.pgn.bz2">') + len('.pgn.bz2')
 
-    url = r.text[urlstart:urlend]
+    url = "http://www.ficsgames.org" + r.text[urlstart:urlend]
 
-    r = requests.get(url)
-    decompressed = bz2.decompress(r.text)
+    data = urllib2.urlopen(url).read()
+    try:
+        if not os.path.exists(("../Games")):
+            os.makedirs("../Games")
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
+
+    open('../Games/%d.pgn.bz2'%year, 'wb').write(data)
+    decompressed = bz2.decompress(data)
     open("../Games/%d.pgn"%year, 'wb').write(decompressed)
